203f2b13bc9ba5a2f514f4c5c0aa404979e99672
Sayantan Guha <sayantanguha@hotmail.com>
2016-04-29 00:59:20 -0700

    Replace character from a string with another string

---
+++ CareerCup/src/Amazon/ReplaceCharFromString.java
@@ -0,0 +1,27 @@
+package Amazon;
+
+public class ReplaceCharFromString {
+
+	public static void main(String[] args) {
+		
+		String s1= "abcedfgchf";
+		String s2= "xyz";
+		char c = 'c';
+		
+		char[] s = s1.toCharArray();
+		String output = "";
+		for (int i =0; i < s.length; i++)
+		{
+			if(s[i]==c)
+			{
+				output = output + s2;
+			}
+			else
+			{
+				output = output + String.valueOf(s[i]);
+			}
+		}
+		System.out.println(output);
+	}
+
+}
