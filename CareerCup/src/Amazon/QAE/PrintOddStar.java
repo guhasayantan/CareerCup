f9585da8f85c14bc7ec8c5a57504d517d9e629cb
Sayantan Guha <sayantanguha@hotmail.com>
2016-04-30 20:39:18 -0700

    Print odd stars

---
+++ CareerCup/src/Amazon/QAE/PrintOddStar.java
@@ -0,0 +1,27 @@
+package Amazon.QAE;
+import java.util.*;
+public class PrintOddStar {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n = scan.nextInt();
+		for(int i=1; i<=n;i=i+2)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");;
+			}
+			System.out.println();;
+		}
+		for(int i=n-2; i>=1;i=i-2)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");;
+			}
+			System.out.println();;
+		}
+	}
+
+}
